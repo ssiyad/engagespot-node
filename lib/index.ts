463dd2de45ff1b9778109ad962e3b39d2b919d77
Sabu Siyad <hello@ssiyad.com>
2022-03-10 17:21:20 +0530

    export config

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@ export * from './engagespot/notification';
 export * from './engagespot/errors/auth-fail.error';
 export * from './engagespot/errors/insufficient-recipients.error';
 export * from './engagespot/types/notification-schema.interface';
+export * from './engagespot/types/config.interface';
